f9b193f79656e7477396583d6defd0481ad1ea42
slayer_0 <87zhaoxiaobo@163.com>
2023-07-27 18:24:18 +0800

    box

--- SoftRayTracer/src/Model/Box.cpp
+++ SoftRayTracer/src/Model/Box.cpp
@@ -1,6 +1,6 @@
 #include "Box.h"
 
-Box::Box(const glm::vec3& center, uint32_t width, uint32_t height, const std::shared_ptr<Material>& mat) :Hittable(mat)
+Box::Box(const glm::vec3& center, uint32_t width, uint32_t height, uint32_t depth, const std::shared_ptr<Material>& mat) :Hittable(mat)
 {
 	
 }
--- SoftRayTracer/src/Model/Box.h
+++ SoftRayTracer/src/Model/Box.h
@@ -7,7 +7,7 @@
 class Box :public Hittable
 {
 public:
-	Box(const glm::vec3& center, uint32_t width, uint32_t height, const std::shared_ptr<Material>& mat);
+	Box(const glm::vec3& center, uint32_t width, uint32_t height, uint32_t depth, const std::shared_ptr<Material>& mat);
 	virtual bool Hit(const Ray& ray, float tMin, float tMax, HitRecord& rec) override;
 	virtual bool GetBoundingBox(BoundingBox& out) override;
 private:
