0a4c9c4b86e5a6e1b17b6b4a74b4bf4f63c0ae76
slayer_0 <87zhaoxiaobo@163.com>
2023-07-27 18:27:16 +0800

    box

--- SoftRayTracer/src/Model/Box.cpp
+++ SoftRayTracer/src/Model/Box.cpp
@@ -2,5 +2,13 @@
 
 Box::Box(const glm::vec3& center, uint32_t width, uint32_t height, uint32_t depth, const std::shared_ptr<Material>& mat) :Hittable(mat)
 {
-	
+	glm::vec3 min = center - glm::vec3(width, height, depth);
+	glm::vec3 max = center + glm::vec3(width, height, depth);
+	aabb = BoundingBox(min, max);
+	//planes
+}
+
+bool Box::Hit(const Ray& ray, float tMin, float tMax, HitRecord& rec)
+{
+	return planes.Hit(ray, tMin, tMax, rec);
 }
